197cad977e6de41ead3839c61f841d057d6454d3
Angel Perez <iAngel.p93@gmail.com>
2025-03-06 14:31:41 +0100

    Latest dotfiles update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -8,8 +8,16 @@ set -gx PATH $PATH $N_PREFIX/bin
 set -gx RUST_PREFIX $HOME/.cargo
 set -gx PATH $PATH $RUST_PREFIX/bin
 
-source ~/.asdf/asdf.fish
+# Add asdf to current PATH
+set -gx PATH $PATH $HOME/bin
+
+# Add asdf data and shims to current PATH
+set -gx ASDF_DATA_DIR $HOME/.asdf
+set -gx PATH $PATH $ASDF_DATA_DIR/shims
+
 source ~/.config/fish/functions/aliases.fish
 
 starship init fish | source
 
+# set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g
